633b39a06f99d454c6e09371f0c588d88f703d12
Kye <kye@klewis.me>
2022-08-08 20:10:00 +1000

    initial

---
+++ README.md
@@ -0,0 +1,6 @@
+# Hello, WASM with C
+
+- Using a recent (year?) LLVM/clang to build a wasm binary.
+
+- @todo explain options
+
---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/fa36ef2f25b1357feb17d7e702c6be4397647ee2";
+  };
+  outputs = { self, nixpkgs }: 
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in with pkgs; {
+      devShells.x86_64-linux.default = mkShell {
+        buildInputs = [ llvm clang nodejs-18_x nodePackages.pnpm ];
+      };
+    };
+}
+    
